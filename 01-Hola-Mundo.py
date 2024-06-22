b84853e48c5b63b48c9254471f17459cb535c599
Humirephp <1510720@senati.pe>
2024-06-22 18:06:22 -0500

    tipo de datos

---
+++ 01-Hola-Mundo.py
@@ -0,0 +1,9 @@
+# Hola Mundo
+print ("Hola Mundo")
+
+# Consultar de tipo de dato 
+print (type ("Hola Mundo")) # Tipo  'str'>
+print (type (5)) # Tipo  'int'>
+print (type (1.5)) # Tipo 'float'>
+print (type(1 + 3j)) # Tipo 'complex'>
+print (type(True)) # Tipo 'bool'>
