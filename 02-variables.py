c55dd2a2ac9579abd0955d83d99f646e2edb6cee
Humirephp <1510720@senati.pe>
2024-06-22 18:05:54 -0500

    variables

---
+++ 02-variables.py
@@ -0,0 +1,24 @@
+# Definir Variables  String
+Nombre = "Mateo"
+print (Nombre)
+# Una forma correcta es tambien 
+mi_nombre_es = "MARCOS"
+print (mi_nombre_es)
+# Definir variable int
+definir_int = 5
+print(definir_int)
+# Convertir un numero entero a una cadena de caravteres string
+definir_str = str(definir_int)
+print(definir_str)
+print(type(definir_str))
+
+# Definir variable bool 
+definir_bool = False
+print (definir_bool)
+
+# Concatenacion de variables en print  
+print(mi_nombre_es, definir_int, definir_bool)
+print("Este es el valor de:", definir_bool)
+
+# Akgunas Funciones del sistema para mostrar el numero de caracteres  
+print (len(mi_nombre_es)) 
