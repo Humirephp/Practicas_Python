387c4448a6a478d15441bef59d98c76c4b7725ea
Humirephp <1510720@senati.pe>
2024-06-23 12:15:52 -0500

    continuara

---
+++ 05-Listas.py
@@ -0,0 +1,21 @@
+## Listas ##
+
+my_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [20 , 30 , 24 , 19]
+print(my_list)
+print(len(my_list))
+
+my_other_list = [24, 1.80, "Humire" , "Elias"]
+print(my_other_list)
+print(type(my_other_list))
+
+print(my_other_list[0])
+print(my_other_list[1])
+print(my_other_list[-1])
+print(my_other_list[-4])
+#print(my_other_list[4]) IndexError
+#print(my_other_list[-5]) IndexError
