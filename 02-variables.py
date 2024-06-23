a8375ec66bb2d60f6842e26420d2bbf45094397a
Humirephp <1510720@senati.pe>
2024-06-23 09:22:12 -0500

    nuevas funciones

--- 02-variables.py
+++ 02-variables.py
@@ -22,3 +22,18 @@ print("Este es el valor de:", definir_bool)
 
 # Akgunas Funciones del sistema para mostrar el numero de caracteres  
 print (len(mi_nombre_es)) 
+
+# Variables en una sola linea 
+edad, dni, apellido, apodo  ="18" , "9999" , "Acero", "Leon"
+print("- mi edad es :", edad, " - mi numero de DNI es :", dni ,"- Mi apellido es :", apellido, " - mi apodo es :" ,apodo)
+
+#Inputs 
+name = input ("¿Cual es tu nombre?")
+age = input ("¿Cual es tu edad?")
+
+print("tu nombre es :", name)
+print("tu edad es :", age) 
+
+# Forzamos el tipo
+address: str = "Esto es elemental"
+print(type(address))
