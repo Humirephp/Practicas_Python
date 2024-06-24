e9aeec4765dd7c6cba6cbb16e40dd8a62f772ac2
Humirephp <1510720@senati.pe>
2024-06-24 12:17:10 -0500

    Listas

--- 05-Listas.py
+++ 05-Listas.py
@@ -5,7 +5,7 @@ my_other_list = []
 
 print(len(my_list))
 
-my_list = [20 , 30 , 24 , 19]
+my_list = [20 , 30 , 24 , 19,20]
 print(my_list)
 print(len(my_list))
 
@@ -17,5 +17,60 @@ print(my_other_list[0])
 print(my_other_list[1])
 print(my_other_list[-1])
 print(my_other_list[-4])
+print(my_list.count(20))
 #print(my_other_list[4]) IndexError
 #print(my_other_list[-5]) IndexError
+ 
+año, altura , apellido, nombre, = my_other_list
+print(nombre)
+
+print(my_list + my_other_list)
+
+my_other_list.append("Ingenieria de Software")
+print(my_other_list)
+
+my_other_list.insert(1, "Azul")
+print(my_other_list)
+
+my_other_list[1] = "negro"
+print(my_other_list)
+
+my_other_list.remove("negro")
+print(my_other_list)
+
+my_list.remove(20)
+print(my_list)
+
+print(my_list.pop(2))
+print(my_list)
+
+my_pop_element = my_list.pop(2)
+print(my_pop_element)
+print(my_list)
+#Eliminar elementos de una lista 
+del my_list[1]
+print(my_list)
+
+#Para copiar unalista antes de eliminarla
+my_new_list= my_list.copy()
+
+my_list.clear()
+print(my_list)
+print(my_new_list)
+
+my_new_list.reverse()
+print(my_new_list)
+
+my_new_list.sort()
+print(my_new_list)
+
+print(my_new_list[1:1])
+
+lista_de_alumnos = ["Juan", "Lucas", "Mateo", "Jesus"]
+print(lista_de_alumnos)
+
+print(lista_de_alumnos.pop(2))
+print(lista_de_alumnos)
+#Eliminar elementos de una lista 
+del lista_de_alumnos[1]
+print(lista_de_alumnos)
